0ea118c0c2aa761cf1b612f4a9b2cbc943bcc18f
pyuung <kyuungah@gmail.com>
2022-05-31 01:33:31 +0900

    feat: :test_tube: lazy에도 테스트 할 수 있는 주석 추가

--- section6.Lazy/lazy.js
+++ section6.Lazy/lazy.js
@@ -3,10 +3,22 @@ import { curry } from './helper.js'
 export const L = {}
 
 L.map = curry(function* (f, iter) {
+  // iter = iter[Symbol.iterator]()
+  // let cur
+  // while (!(cur = iter.next()).done) {
+  //   const a = cur.value
+  //   yield f(a)
+  // }
   for (const a of iter) yield f(a)
 })
 
 L.filter = curry(function* (f, iter) {
+  // iter = iter[Symbol.iterator]()
+  // let cur
+  // while (!(cur = iter.next()).done) {
+  //   const a = cur.value
+  //   if (f(a)) yield a
+  // }
   for (const a of iter) if (f(a)) yield a
 })
 
